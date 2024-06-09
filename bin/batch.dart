a75acf215b8d0c3f15c3598d51269bbef641780e
Brice Delvallee <brice.delvallee@gmail.com>
2024-06-09 16:31:14 +0300

    batch tool to run a serie of problems

---
+++ bin/batch.dart
@@ -0,0 +1,43 @@
+// Solves all days/parts of a specific year
+// usage: dart bin/year.dart 2023
+// Note: make sure you have downloaded the input files using get_inputs.dart
+import 'dart:io';
+
+const int minPb = 1;
+const int maxPb = 900;
+const int nbPerFolder = 25;
+
+void main(List<String> arguments) async {
+  int? pbStart;
+  int? pbEnd;
+  if (arguments.length >= 2) {
+    pbStart = int.tryParse(arguments[0]);
+    pbEnd = int.tryParse(arguments[1]);
+  }
+  if ((pbStart == null) ||
+      (pbEnd == null) ||
+      (pbStart < 1) ||
+      (pbEnd < pbStart)) {
+    print('Usage: dart bin/batch.dart 1 25');
+    print('arguments are starting and ending problem number');
+    return;
+  }
+
+  // execute a shell 'dart run' on each day[1-25] and each part[1-2] until an error is encountered
+  for (int i = pbStart; i <= pbEnd; i++) {
+    int firstFolder = (((i - 1) ~/ nbPerFolder) * nbPerFolder) + 1;
+    String folder =
+        '${firstFolder.toString().padLeft(3, '0')}-${(firstFolder + nbPerFolder - 1).toString().padLeft(3, '0')}';
+    String file = './bin/$folder/euler${i.toString().padLeft(3, '0')}.dart';
+    if (File(file).existsSync()) {
+      ProcessResult results = await Process.run('dart', [file],
+          runInShell: true, workingDirectory: '.');
+      if (results.exitCode != 0) {
+        print(results.stderr);
+        exit(-1);
+      } else {
+        print(results.stdout);
+      }
+    }
+  }
+}
